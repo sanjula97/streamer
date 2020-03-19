457c0de35518bc41f0f3cf734932f06c9b8ead77
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-03-19 15:53:41 +0530

    updated delete component

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -13,7 +13,7 @@ class StreamList extends React.Component {
             return (
                 <div className="right floated content">
                     <Link to={`/streams/edit/${stream.id}`} className="ui button primary">EDIT</Link>
-                    <button className="ui button negarive">DELETE</button>
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negarive">DELETE</Link>
                 </div>
             );
         }
